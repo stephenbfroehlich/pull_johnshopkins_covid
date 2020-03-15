d9ed584c119b7df2d5a1f8fe09d0911f4794bed0
Stephen Froehlich <stephen.froehlich@gmail.com>
2020-03-15 07:54:35 -0600

    Adding a basic graph of new cases.

--- pull_johnshopkins_covid.R
+++ pull_johnshopkins_covid.R
@@ -10,9 +10,14 @@ jh_melt_format <-
                 names_to = "obs_date",
                 values_to = "cases"
             ) %>%
-            mutate(obs_date = as.Date(obs_date, format = "%m/%d/%y"))
+            mutate(obs_date = as.Date(obs_date, format = "%m/%d/%y")) %>%
+            clean_names()
     }
 
+new_cases <-
+    function(c) {
+        pmax(c - if_else(is.na(lag(c)), 0 , lag(c)), 0)
+    }
 
 confirmed <-
     "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv" %>%
@@ -33,15 +38,45 @@ recovered <-
     url() %>%
     read_csv() %>%
     jh_melt_format() %>%
+
     filter(cases > 0)
 
 
-confirmed %>%
-    filter(`Province/State` == "Colorado")
 
-deaths %>%
-    filter(`Province/State` == "Colorado")
+(
+    confirmed_colo <-
+        confirmed %>%
+        filter(province_state == "Colorado") %>%
+        mutate(new_cases = new_cases(cases),
+               category = "Cases")
+)
 
-recovered %>%
-    filter(`Province/State` == "Colorado")
+(
+    deaths_colo <-
+        deaths %>%
+        filter(province_state == "Colorado") %>%
+        mutate(new_cases = new_cases(cases),
+               category = "Deaths")
+)
+
+(
+    recovered_colo <- recovered %>%
+        filter(province_state == "Colorado") %>%
+        mutate(new_cases = new_cases(cases),
+               category = "Recoveries")
+)
+
+list(confirmed_colo,
+     deaths_colo,
+     recovered_colo) %>%
+    bind_rows() %>%
+    ggplot() +
+    geom_bar(
+        aes(x = obs_date,
+            y = new_cases,
+            fill = category),
+        stat = "identity",
+        alpha = .5,
+        position = position_dodge(width = 0)
+    )
 
